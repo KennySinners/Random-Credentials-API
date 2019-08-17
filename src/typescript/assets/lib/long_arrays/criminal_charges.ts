89c94adb880f26e3dc95d72fe7d461cfc64a7730
KennySinners <odeukuracsve@gmail.com>
2019-08-17 23:30:37 +0200

    More long arrays added

---
+++ src/typescript/assets/lib/long_arrays/criminal_charges.ts
@@ -0,0 +1,317 @@
+export let crimecharges = [ {
+
+"Aggravated Assault": {
+      charge: "Aggravated Assault",
+      description: []
+},
+
+"Insurance Fraud": {
+      charge: "Insurance Fraud",
+      description: []
+},
+
+"Aiding and Abetting / Accessory": {
+      charge: "Aiding and Abetting / Accessory",
+      description: []
+},
+
+"Kidnapping": {
+      charge: "Kidnapping",
+      description: []
+},
+
+"Arson": {
+      charge: "Arson",
+      description: []
+},
+
+"Manslaughter: Involuntary": {
+      charge: "Manslaughter: Involuntary",
+      description: []
+},
+
+"Assault / Battery": {
+      charge: "Assault / Battery",
+      description: []
+},
+
+"Manslaughter: Voluntary": {
+      charge: "Manslaughter: Voluntary",
+      description: []
+},
+
+"Attempted Murder": {
+      charge: "Attempted Murder",
+      description: []
+},
+
+"Illegal possesion of Marijuana": {
+      charge: "Illegal possession of Marijuana",
+      description: []
+},
+
+"Bribery": {
+      charge: "Bribery",
+      description: []
+},
+
+"MIP: A Minor in Possession": {
+      charge: "MIP: A Minor in Possession",
+      description: []
+},
+
+"Burglary": {
+      charge: "Burglary",
+      description: []
+},
+
+"Money Laundering": {
+      charge: "Money Laundering",
+      description: []
+},
+
+"Child Abandonment": {
+      charge: "Child Abandonment",
+      description: []
+},
+
+"Murder: First - degree": {
+      charge: "Murder: First - degree",
+      description: []
+},
+
+"Child Abuse": {
+      charge: "Child Abuse",
+      description: []
+},
+
+"Murder: Second - degree": {
+      charge: "Murder: Second - degree",
+      description: []
+},
+
+"Child Pornography": {
+      charge: "Child Pornography",
+      description: []
+},
+
+"Open Container(of alcohol)": {
+      charge: "Open Container (of alcohol)",
+      description: []
+},
+
+"Computer Crime": {
+      charge: "Computer Crime (Hacking)",
+      description: []
+},
+
+"Perjury": {
+      charge: "Perjury",
+      description: []
+},
+
+"Conspiracy": {
+      charge: "Conspiracy",
+      description: []
+},
+
+"Probation Violation": {
+      charge: "Probation Violation",
+      description: []
+},
+
+"Credit / Debit Card Fraud": {
+      charge: "Credit / Debit Card Fraud",
+      description: []
+},
+
+"Prostitution": {
+      charge: "Prostitution",
+      description: []
+},
+
+"Criminal Contempt of Court": {
+      charge: "Criminal Contempt of Court",
+      description: []
+},
+
+"Public Intoxication": {
+      charge: "Public Intoxication",
+      description: []
+},
+
+"Cyberbullying": {
+      charge: "Cyberbullying",
+      description: []
+},
+
+"Pyramid Schemes": {
+      charge: "Pyramid Schemes",
+      description: []
+},
+
+"Disorderly Conduct": {
+      charge: "Disorderly Conduct",
+      description: []
+},
+
+"Racketeering / RICO": {
+      charge: "Racketeering / RICO",
+      description: []
+},
+
+"Disturbing the Peace": {
+      charge: "Disturbing the Peace",
+      description: []
+},
+
+"Rape": {
+      charge: "Rape",
+      description: []
+},
+
+"Domestic Violence": {
+      charge: "Domestic Violence",
+      description: []
+},
+
+"Robbery": {
+      charge: "Robbery",
+      description: []
+},
+
+"Drug Manufacturing and Cultivation": {
+      charge: "Drug Manufacturing and Cultivation",
+      description: []
+},
+
+"Securities Fraud": {
+      charge: "Securities Fraud",
+      description: []
+},
+
+"Drug Possession": {
+      charge: "Drug Possession",
+      description: []
+},
+
+"Sexual Assault": {
+      charge: "Sexual Assault",
+      description: []
+},
+
+"Drug Trafficking / Distribution": {
+      charge: "Drug Trafficking / Distribution",
+      description: []
+},
+
+"Shoplifting": {
+      charge: "Shoplifting",
+      description: []
+},
+
+"DUI / DWI": {
+      charge: "DUI / DWI",
+      description: []
+},
+
+"Solicitation": {
+      charge: "Solicitation",
+      description: []
+},
+
+"Embezzlement": {
+      charge: "Embezzlement",
+      description: []
+},
+
+"Stalking": {
+      charge: "Stalking",
+      description: []
+},
+
+"Extortion": {
+      charge: "Extortion",
+      description: []
+},
+
+"Statutory Rape": {
+      charge: "Statutory Rape",
+      description: []
+},
+
+"Forgery": {
+      charge: "Forgery",
+      description: []
+},
+
+"Tax Evasion / Fraud": {
+      charge: "Tax Evasion / Fraud",
+      description: []
+},
+
+"Fraud": {
+      charge: "Fraud",
+      description: []
+},
+
+"Telemarketing Fraud": {
+      charge: "Telemarketing Fraud",
+      description: []
+},
+
+"Harassment": {
+      charge: "Harassment",
+      description: []
+},
+
+"Theft": {
+      charge: "Theft",
+      description: [
+            "Stole several T-Shirts from a Retail Shop."
+      ]
+},
+
+"Hate Crimes": {
+      charge: "Hate Crimes",
+      description: [
+            "Called a Black Male a 'n***er' and proceeded to beat him up. The suspect said that he did it because: 'he looked at me weirdly'."
+      ]
+},
+
+"Vandalism": {
+      charge: "Vandalism",
+      description: [
+            "Wrote 'ACAB' on a public school with spray paint."
+      ]
+},
+
+"Homicide": {
+      charge: "Homicide",
+      description: [
+            "Shot his ex-wife at point-blank range; 'Execution' style."
+      ]
+},
+
+"Identity Theft": {
+      charge: "Identity Theft",
+      description: [
+            "Tried crossing the border with an ID as 'Max DeVito'."
+      ]
+},
+
+"Wire Fraud": {
+      charge: "Wire Fraud",
+      description: [
+            "Leaking information about a multi-million company. Evidence found on recordings that were done through a wire."
+      ]
+},
+
+"Indecent Exposure": {
+      charge: "Indecent Exposure",
+      description: [
+            "Stripped down, naked. Screamed 'I am Batman' and proceeded to do a 'helicopter' move with his penis."
+      ]
+}
+
+}];
