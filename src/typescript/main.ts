2c16fc85533a17ac2c656fb60809641f4367dce4
KennySinners <odeukuracsve@gmail.com>
2019-08-17 23:30:11 +0200

    Updated the API, more detailed information, no need to input manual information, API randomly generates everything.

--- src/typescript/main.ts
+++ src/typescript/main.ts
@@ -1,74 +1,119 @@
-const { rword } = require("rword");
-const { ems, emis } = require("./assets/lib/long_arrays/emails");
-const { gen } = require("./gen");
-const { names } = require("./assets/lib/long_arrays/names");
-const { dogs } = require("./assets/lib/long_arrays/dogs");
+import { maleNames } from "./assets/lib/long_arrays/male";
+import { femaleNames } from "./assets/lib/long_arrays/female";
+import { surnames } from "./assets/lib/long_arrays/surname";
+import { gen } from "./gen";
+import { dogs } from "./assets/lib/long_arrays/dogs";
+import { rword } from "rword";
+import { ems, emis } from "./assets/lib/long_arrays/emails";
 const array = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
-export function Info(person: { name?: String, surname?: String, age?: Number }, jobs?: Array<string>, family?: Boolean, wife?: String, pets?: Array<Object>, objKids?: Array<Object>) {
+const information =
+    async (person?: { name?: string, surname?: string, age?: number, sex?: string, sexuality?: string, family?: boolean }, spouse?: { name?: string, surname?: string, age?: number, sex?: string, sexuality?: string }, kids: Array<object> = [], pets: Array<object> = []) => {
 
-      const words = rword.generate(2).join("");
-      const nums = array[Math.floor(Math.random() * array.length)];
-      const email = `${words}${nums}@${ems[Math.floor(Math.random() * ems.length)]}`;
-      const secondEmail = `${words}${nums}@${emis[Math.floor(Math.random() * emis.length)]}`;
-      const generated = gen(18);
-      const anothergen = gen(18);
-      const fam = family === true ? "This person has a family" : "This person does not have a family";
+        const words = rword.generate(2).toString().split(",").join("");
+        const word = rword.generate(2).toString().split(",").join("");
+        const nums = array[Math.floor(Math.random() * array.length)];
+        const email = `${words}${nums}@${ems[Math.floor(Math.random() * ems.length)]}`;
+        const secondEmail = `${word}${nums}@${emis[Math.floor(Math.random() * emis.length)]}`;
+        const generated = gen(18);
+        const anothergen = gen(18);
+        const num = 5;
+        const petNum = Math.floor(Math.random() * 7);
+        let kid = {
+            sex: ''
+        }
 
-      for (let i = 0; i < objKids.length; i++) {
-            const randomAge = Math.floor(Math.random() * [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18].length);
-            objKids[i] = {
-                  name: names[Math.floor(Math.random() * names.length)],
-                  surname: person.surname || "Doe",
-                  age: randomAge === 0 ? 5 : randomAge
+        if (!person) {
+            person = {
+                name: maleNames[Math.floor(Math.random() * maleNames.length)],
+                surname: surnames[Math.floor(Math.random() * surnames.length)],
+                age: 27,
+                sex: ["Male", "Female"][Math.floor(Math.random() * ["Male", "Female"].length)],
+                family: true
             }
-      }
+        }
 
-      for (let y = 0; y < pets.length; y++) { 
-            const randomAge = Math.floor(Math.random() * 16);
-            pets[y] = { 
-                  name: dogs[Math.floor(Math.random() * dogs.length)],
-                  age: randomAge === 0 ? 1 : randomAge
+        if (person.family === true) {
+            if (!spouse) {
+                spouse = {
+                    name: femaleNames[Math.floor(Math.random() * femaleNames.length)],
+                    surname: person.surname,
+                    age: person.age - 3,
+                    sex: ["Male", "Female"][Math.floor(Math.random() * ["Male", "Female"].length)]
+                }
             }
-      }
 
-      if (family === true) {
+            if (!kids.length) {
 
-            return {
-                  Person: {
-                        Name: person.name === "" || person.name === undefined ? person.name = "Jon" : person.name,
-                        Surname: person.surname === "" || person.surname === undefined ? person.surname = "Doe" : person.surname,
-                        Age: person.age || 25,
-                        Email: email,
-                        ID: generated
-                  }, Jobs: jobs, Family: fam, About_Family: {
-                        Wife: {
-                              Name: wife !== undefined ? wife : "No Wife",
-                              Surname: person.surname,
-                              Age: Number(person.age) - 2 || 25 - 2,
-                              Email: secondEmail,
-                              ID: anothergen
-                        },
-                        Kids: {
-                              Number_of_kids: objKids.length,
-                              About_Kids: objKids
-                              
-                        },
-                        Pets: {
-                              Number_of_pets: pets.length,
-                              About_pets: pets
-                        }
-                  }
-            };
-      } else {
-            return {
-                  Person: {
-                        Name: person.name === "" || person.name === undefined ? "Jon" : person.name,
-                        Surname: person.surname === "" || person.surname === undefined ? "Doe" : person.surname,
-                        Age: person.age,
-                        Email: email,
-                        ID: gen(18)
-                  }, Jobs: jobs.join(", "), Family: "This person does not have a family"
-            };
-      };
-};
+                for (let i = 0; i < num; i++) {
+                    kids.push({
+                        name: kid.sex.toLowerCase() === "female" ? femaleNames[Math.floor(Math.random() * femaleNames.length)] : maleNames[Math.floor(Math.random() * maleNames.length)],
+                        surname: person.surname,
+                        sex: ["Male", "Female"][Math.floor(Math.random() * ["Male", "Female"].length)],
+                        age: person.age >= 27 && person.age <= 35 ? [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12][Math.floor(Math.random() * [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0].length)] : [13, 14, 15, 16, 17, 18, 19, 20][Math.floor(Math.random() * [13, 14, 15, 16, 17, 18, 19, 20].length)]
+                    })
+                }
+            }
+
+            if (!pets.length) {
+
+                for (let i = 0; i < petNum; i++) {
+                    pets.push({
+                        name: dogs[Math.floor(Math.random() * dogs.length)],
+                        age: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15][Math.floor(Math.random() * [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].length)],
+                        sex: ["Male", "Female"][Math.floor(Math.random() * ["Male", "Female"].length)]
+                    })
+                }
+
+            }
+        }
+
+        if (person.sex === spouse.sex) {
+            person.sexuality = "Homosexual";
+            spouse.sexuality = "Homosexual";
+        } else {
+            person.sexuality = "Heterosexual";
+            spouse.sexuality = "Heterosexual";
+        }
+
+
+        const object = {
+            Person: {
+                Name: person.name,
+                Surname: person.surname,
+                Age: person.age,
+                Family: person.family === true ? "Has a family" : "Doesn't have a family",
+                Sex: person.sex,
+                Sexuality: person.sexuality,
+                Email: email,
+                ID: generated
+            },
+            About_Family: {
+                Spouse: {
+                    Name: spouse.name ? spouse.name : "No spouse",
+                    Surname: spouse.surname,
+                    Age: Number(person.age) - 2 || 25 - 2,
+                    Sex: person.sex,
+                    Sexuality: person.sexuality,
+                    Email: secondEmail,
+                    ID: anothergen
+                },
+                Kids: {
+                    Number_of_kids: !kids.length ? "No kids" : kids.length,
+                    About_Kids: kids
+
+                },
+                Pets: {
+                    Number_of_pets: !pets.length ? "No pets" : pets.length,
+                    About_pets: pets
+                }
+            }
+        }
+
+        return new Promise((res, rej) => {
+            res(object);
+            rej(object);
+        })
+    }
+
+export { information };
